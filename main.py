8fbd0fed1a80778a0bc0d5dd3d2ff36b295bdb30
Silvio Lima <silviolima07@gmail.com>
2024-02-14 17:08:17 -0300

    First commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Document
+
 from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+
 # from PyPDF2 import PdfReader
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
